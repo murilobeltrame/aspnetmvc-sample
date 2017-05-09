83c03c64a49b905d8f00115e01437edbab085f08
Murilo Beltrame <murilobeltrame@gmail.com>
2017-05-08 22:57:28 -0300

    First Commit

---
+++ SampleApplication/SampleApplication.UI.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleApplication.UI.Web.MvcApplication" Language="C#" %>
---
+++ SampleApplication/SampleApplication.UI.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleApplication.UI.Web.Startup))]
+namespace SampleApplication.UI.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
